d8960bafff1c9fd98df546e0e98c0bf26897889a
hinesqui <hinesqui@gmail.com>
2020-07-30 09:57:52 +0300

    Small edit log.info

--- src/main/java/ru/nesqui/gateway_jira_bot/services/YandexService.java
+++ src/main/java/ru/nesqui/gateway_jira_bot/services/YandexService.java
@@ -3,17 +3,11 @@ package ru.nesqui.gateway_jira_bot.services;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import ru.nesqui.gateway_jira_bot.entities.IAMTokenEntity;
-import ru.nesqui.gateway_jira_bot.entities.VoiceMessageEntity;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -35,11 +29,12 @@ public class YandexService {
         valueMap.put("yandexPassportOauthToken", "AgAAAAAI1CYzAATuwYhASUaHQk6TjHmsSoSqGJM"); //TODO: разобраться почему переменная token = null
     }
 
+    // Update yandex AIM token
     @Scheduled(fixedRate = 11 * 60 * 60 * 1000)
     public void updateIAMToken() {
         ResponseEntity<IAMTokenEntity> response =  this.restTemplate.postForEntity(yandexUrl, valueMap, IAMTokenEntity.class);
         this.receivedToken = Objects.requireNonNull(response.getBody()).getIamToken();
-        log.info("Token has updated");
+        log.info("Yandex token has updated");
     }
 
     public String getIAMToken() {
